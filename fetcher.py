39241211cb05f532021b23ca8db589770ae6ad3c
aroussel-fenix <aroussel@fenixintl.com>
2019-09-27 15:40:54 +0300

    added comment to fetcher

--- fetcher.py
+++ fetcher.py
@@ -1,5 +1,7 @@
 from kinesis_demo import client
 
+# This is just here if need to fetch some records from the stream, but will probably remove.
+
 shard_it = client.get_shard_iterator(StreamName='demo_stream', ShardId='shardId-000000000000',
                                      ShardIteratorType='LATEST')
 
